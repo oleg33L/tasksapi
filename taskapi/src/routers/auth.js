e90b236b3222371fa0906c84bb9361765de567cb
oleg33L <vzolss@gmail.com>
2024-10-30 15:12:15 +0200

    fixed files

--- taskapi/src/routers/auth.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express');
-const router = Router();
-const { login, register } = require('../controllers/auth.js');
-
-router.post("/register", register);
-router.post('/login', login);
-
-module.exports = router;
